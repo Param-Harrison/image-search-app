c0a1bb7700201078eddd6421abb259c8261c4a28
Param-Harrison <paramanantham@live.com>
2019-03-22 18:27:43 +0200

    feat: complete search form with react hooks

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchForm.css';
 
 const SearchForm = () => {
+    const defaultSearchValue = '';
+    const [inputValue, setInputValue] = useState(defaultSearchValue);
+    const [search, setSearch] = useState(defaultSearchValue);
+
+    const onInputChange = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    const onSearchSubmit = (e) => {
+        e.preventDefault();
+        setSearch(inputValue);
+        console.log(search);
+    }
+
     return (
         <section className="SearchForm">
-            <form className="SearchForm-form">
-                <input placeholder="search images - laptops, wallpapers, etc" type="search" />
+            <div className="SearchForm-content">
+                <h1>Asgard</h1>
+                <p>Image search using unsplash API</p>
+            </div>
+            <form className="SearchForm-form" onSubmit={onSearchSubmit}>
+                <input
+                    placeholder="search images - laptops, wallpapers, etc" 
+                    type="search"
+                    value={inputValue}
+                    onChange={onInputChange}
+                />
                 <button type="submit">Search</button>
             </form>
         </section>
