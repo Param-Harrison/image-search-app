64498ea79d2bdff626d953dbdb8ed234c7b0aa29
Param-Harrison <paramanantham@live.com>
2019-03-23 19:47:28 +0200

    feat: 404 page with styles

--- src/pages/NoMatchPage/NoMatchPage.js
+++ src/pages/NoMatchPage/NoMatchPage.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './NoMatchPage.css';
 
 const NoMatchPage = () => {
-    return <div>No Matching Route</div>;
+    return (
+        <section className="NoMatchPage">
+            <div className="NoMatchPage-container">
+                <h1 className="NoMatchPage-title">
+                    404 - Loki will help you find Asgard
+                </h1>
+                <p className="NoMatchPage-description">
+                    Thanks for trying my ebook, Keep learning and help me
+                    improve my teaching
+                </p>
+                <Link className="NoMatchPage-link" to="/">
+                    Go to Main Page
+                </Link>
+            </div>
+        </section>
+    );
 };
 
 export default NoMatchPage;
