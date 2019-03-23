fe33d201735ee68e66b6be1e94ff86e0870e073e
Param-Harrison <paramanantham@live.com>
2019-03-23 09:50:08 +0200

    feat: route with index page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 import IndexPage from './pages/IndexPage';
 
 const App = () => (
-    <div className="App">
-        <IndexPage />
-    </div>
+    <Router>
+        <div className="App">
+            <Route path="/" exact component={IndexPage} />
+        </div>
+    </Router>
 );
 
 export default App;
