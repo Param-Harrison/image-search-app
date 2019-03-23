14874b2274d8a3fdc2f2c25d67d5b1d30a10d9de
Param-Harrison <paramanantham@live.com>
2019-03-23 19:19:47 +0200

    feat: no match page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import IndexPage from './pages/IndexPage';
 import SearchPage from './pages/SearchPage';
+import NoMatchPage from './pages/NoMatchPage';
 
 const App = () => (
     <Router>
         <div className="App">
-            <Route path="/" exact component={IndexPage} />
-            <Route path="/search/:searchText" component={SearchPage} />
+            <Switch>
+                <Route path="/" exact component={IndexPage} />
+                <Route path="/search/:searchText" component={SearchPage} />
+                <Route component={NoMatchPage} />
+            </Switch>
         </div>
     </Router>
 );
---
+++ src/pages/NoMatchPage/NoMatchPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './NoMatchPage.css';
+
+const NoMatchPage = () => {
+    return <div>No Matching Route</div>;
+};
+
+export default NoMatchPage;
