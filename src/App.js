7f8d5b2fac02aa13f35e10d29613734b711a52b9
Paramanantham Harrison <paramanantham@live.com>
2019-03-23 17:54:41 +0200

    feat: react router to create pages - search and index page (#4)
    * feat: route with index page
    * feat: added new search page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 import IndexPage from './pages/IndexPage';
+import SearchPage from './pages/SearchPage';
 
 const App = () => (
-    <div className="App">
-        <IndexPage />
-    </div>
+    <Router>
+        <div className="App">
+            <Route path="/" exact component={IndexPage} />
+            <Route path="/search" component={SearchPage} />
+        </div>
+    </Router>
 );
 
 export default App;
