25ed95dd7b8ff55e1cbe8a43659401f84f0e1257
Param-Harrison <paramanantham@live.com>
2019-03-23 17:50:40 +0200

    feat: added new search page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import IndexPage from './pages/IndexPage';
+import SearchPage from './pages/SearchPage';
 
 const App = () => (
     <Router>
         <div className="App">
             <Route path="/" exact component={IndexPage} />
+            <Route path="/search" component={SearchPage} />
         </div>
     </Router>
 );
---
+++ src/pages/SearchPage/SearchPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SearchPage.css';
+
+const SearchPage = () => {
+    return <div>Search Page</div>;
+};
+
+export default SearchPage;
