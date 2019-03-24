4111f88105d79323500a822b6f3d72f55fc8f6c3
Paramanantham Harrison <paramanantham@live.com>
2019-03-24 09:03:43 +0200

    feat: basic pixabay api calls (#8)
    * chore: add .env file
    * feat: show photos with pixabay api

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 import './SearchPage.css';
 
-const SearchPage = ({ match }) => {
+const SearchPage = ({
+    match: {
+        params: { searchText },
+    },
+}) => {
+    const initialResult = { hits: [] };
+    const [data, setData] = useState(initialResult);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                `https://pixabay.com/api/?` +
+                    `key=${process.env.REACT_APP_PIXABAY_API_KEY}&` +
+                    `q=${encodeURIComponent(searchText)}&` +
+                    `page=1&` +
+                    `image_type=photo&pretty=true&safesearch=true`
+            );
+
+            setData(result.data);
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
-            Search for <strong>{match.params.searchText}</strong>
+            Search result for <strong>{searchText}</strong>
+            {data &&
+                data.hits &&
+                data.hits.length > 0 &&
+                data.hits.map((image, index) => (
+                    <div key={index}>
+                        <img
+                            src={image.previewURL}
+                            alt={`${searchText} - ${index}`}
+                        />
+                        <div>{image.user}</div>
+                    </div>
+                ))}
         </div>
     );
 };
